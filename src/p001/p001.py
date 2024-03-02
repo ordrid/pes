2590406023c5b8f20642388b3f7059af0134b411
Michael B. Espeña <espena.michael.pub@gmail.com>
2024-03-02 13:57:06 +0800

    [py] add python solution to problem1

---
+++ src/p001/p001.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+def generator_solution():
+    return sum(n for n in range(3, 1000) if n % 3 == 0 or n % 5 == 0)
+
+
+def series_solution():
+    def sum_multiples_of(num):
+        number_of_multiples = 999 // num
+        return number_of_multiples * (number_of_multiples + 1) * num // 2
+
+    return sum_multiples_of(3) + sum_multiples_of(5) - sum_multiples_of(15)
+
+
+print(series_solution())
+print(generator_solution())
