fdd2d6428cbc55c1f15e0273f2443ded4993c2bf
Michael B. Espeña <espena.michael.pub@gmail.com>
2024-03-02 15:40:32 +0800

    [hs] add haskell solution to problem 6

---
+++ src/p006/p006.hs
@@ -0,0 +1,16 @@
+module P006 where
+
+sumOfSquares :: Integer -> Integer
+sumOfSquares n = n * (n + 1) * (2 * n + 1) `div` 6
+
+squareOfSum :: Integer -> Integer
+squareOfSum n =
+  let arithmeticSum = n * (n + 1) `div` 2
+   in arithmeticSum ^ 2
+
+seriesSolution :: Integer -> Integer
+seriesSolution n = squareOfSum n - sumOfSquares n
+
+main :: IO ()
+main = do
+  print $ seriesSolution 100
