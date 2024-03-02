e088b8b71dcc01dbc52f720691d814dcd438f536
Michael B. Espeña <espena.michael.pub@gmail.com>
2024-03-02 16:38:26 +0800

    [hs] use where in haskell solution for problem 6
    I think using where in this case is a bit more appropriate.

--- src/p006/p006.hs
+++ src/p006/p006.hs
@@ -4,9 +4,9 @@ sumOfSquares :: Integer -> Integer
 sumOfSquares n = n * (n + 1) * (2 * n + 1) `div` 6
 
 squareOfSum :: Integer -> Integer
-squareOfSum n =
-  let arithmeticSum = n * (n + 1) `div` 2
-   in arithmeticSum ^ 2
+squareOfSum n = arithmeticSum ^ 2
+  where
+    arithmeticSum = n * (n + 1) `div` 2
 
 seriesSolution :: Integer -> Integer
 seriesSolution n = squareOfSum n - sumOfSquares n
